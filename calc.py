1a800e746b703e77a0f448b776c08a02226135e5
Marquezliu-lq <84384725+Marquezliu-lq@users.noreply.github.com>
2023-07-05 21:10:59 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -78,26 +78,21 @@ def getAbanEmi(UID,year,isFlood,flag):
 
 #AMM from mines closed before 2011
 def iniAbanEmi(year,city):
-    '''计算某个特定行政区，2011前废弃煤矿的排放'''
-    '''针对整个行政区的调用函数'''
-    '''返回单位kg'''
     aveFac=dfAbanEf.loc[city,'排放因子']
-    iniEmi=aveProd*aveFac#单个的初始排放
+    iniEmi=aveProd*aveFac
     totEmi=0
     for i in range(2005,2011):
         num=dfAbanNum.loc[city,i]
         #dry
         emiDry=num*iniEmi*math.pow(1+b*Dd*(year-i),(-1/b))*((1-floodRate))
-        #水淹
+        #flooded
         emiFlood=num*iniEmi*math.exp(-(year-i)*Df)*floodRate
         
         totEmi=totEmi+emiFlood+emiDry
     return totEmi
 
-#矿后活动
+#Post-mining emissions
 def getResEmi(UID,year):
-    '''传入具体煤矿的UID，计算对应的矿后活动排放'''
-    '''注意只计算井工矿的矿后活动'''
     judge=dfTot.loc[UID,'是否露天']
     if judge:
         Efs=resPit*0.67
@@ -111,9 +106,8 @@ def getResEmi(UID,year):
     emi=prod*12*Efs
     return emi
 
-#回收利用
+#recovery and utilization
 def getRevEmi(UID,year):
-    '''传入UID，根据年度数据调整具体到煤矿的回收利用'''
     city=dfTot.loc[UID,'行政区']
     global dfMineRes
     totEmi=dfMineRes.loc[city,year]#unit 吨
@@ -121,68 +115,40 @@ def getRevEmi(UID,year):
     oriEmi=getMineEmi(UID, year)
     return oriEmi*fac
 
-#定义初始化程序
-def init():
-    yearList=[2011,2012,2013,2014,2015,2016,2017,2018,2019]
-    for year in yearList:
-        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>=year)&(dfTot['是否露天']==0)]
-        for key in ['突出','高瓦斯','瓦斯']:
-            for city in citys:
-                dfTemp1=dfTemp[(~pd.isna(dfTemp['排放因子']))&(dfTemp['瓦斯等级']==key)&(dfTemp['行政区']==city)&(~pd.isna(dfTemp['月产煤量']))]#排放因子不为空
-                dfTemp2=dfTemp[(pd.isna(dfTemp['排放因子']))&(dfTemp['瓦斯等级']==key)&(dfTemp['行政区']==city)]#排放因子为空
-                if dfTemp2.empty:
-                    continue
-                totEmi=0
-                UIDList=dfTemp1.index.tolist()
-                for UID in UIDList:
-                    totEmi=totEmi+dfTot.loc[UID,'排放因子']*dfPro.loc[UID,year]
-                totPro=getProdAbi(UIDList,year)
-                if totPro==0:
-                    continue
-                emiFac=totEmi/totPro
-                idxList=dfTemp2.index.tolist()
-                for i in idxList:
-                    dfTot.loc[i,'排放因子']=emiFac
-    dfTot.to_excel(r'所有煤矿总表.xlsx',sheet_name='Sheet1')
 
-# #主体计算程序
 if __name__=='__main__':
-    '''主体计算程序'''
-    #根据情况是否初始化
-    #init()#初始化填补空缺Efs
-    #先计算井工部分
     global dfMineRes
-    #以下为国家级和省级排放计算
+    #provincial and national emissions
     dfMineRes=pd.DataFrame(index=citys,columns=timeSeries+['单位'])
-    dfMineRes['单位']='吨'
-    #以下为矿井级别的排放计算
+    dfMineRes['单位']='吨'# unit t
+    #mine-level emissions
     dfMine=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfMine['lng']=dfTot['经度_百度_POI']
     dfMine['lat']=dfTot['纬度_百度_POI']
     dfMine['单位']='百吨'
     for year in timeSeries:
-        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==0)]#筛选每年井工表
+        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==0)]#sheet of in-production mines in that year
         for city in citys:
             dfT=dfTemp[dfTemp['行政区']==city]
             UIDlist=dfT.index.tolist()
             emi=0
             for UID in UIDlist:
-                emiTemp=getMineEmi(UID, year)#返回单位为 kg
+                emiTemp=getMineEmi(UID, year)#unit kg
                 emi=emi+emiTemp
-                dfMine.loc[UID,year]=emiTemp/100000#单位  百吨
-            dfMineRes.loc[city,year]=emi/1000#unit 吨
+                dfMine.loc[UID,year]=emiTemp/100000#unit 100t
+            dfMineRes.loc[city,year]=emi/1000#unit t
         dfMineRes.loc['全国',year]=dfMineRes[year].sum()/1000000#unit Mt或Tg
         dfMineRes.loc['全国','单位']='Tg'
-    #再计算露天部分
+    #open-pit mines
     dfPitRes=pd.DataFrame(index=citys,columns=timeSeries+['单位'])
     dfPitRes['单位']='吨'
-    #矿井级别
+    #mine-level
     dfPit=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfPit['lng']=dfTot['经度_百度_POI']
     dfPit['lat']=dfTot['纬度_百度_POI']
     dfPit['单位']='百吨'
     for year in timeSeries:
-        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==1)]#筛选每年露天表
+        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==1)]#sheet of open-pit mines
         for city in citys:
             dfT=dfTemp[dfTemp['行政区']==city]
             UIDlist=dfT.index.tolist()
@@ -190,57 +156,45 @@ if __name__=='__main__':
             for UID in UIDlist:
                 emiTemp=getPitEmi(UID, year)
                 emi=emi+emiTemp
-                dfPit.loc[UID,year]=emiTemp/100000#单位  百吨
+                dfPit.loc[UID,year]=emiTemp/100000#unit 100t
             dfPitRes.loc[city,year]=emi/1000#unit 吨
         dfPitRes.loc['全国',year]=dfPitRes[year].sum()/1000000#unit Mt或Tg
         dfPitRes.loc['全国','单位']='Tg'
-    #计算废弃部分
-    
-    ###首先随机生成判断是否水淹
-    ##原表中有提供默认的是否水淹
-    # idxList=dfTot.index.tolist()
-    # for i in idxList:
-    #     flag=random.random()
-    #     if flag>floodRate:
-    #         #未被水淹
-    #         dfTot.loc[i,'是否水淹']=0#表示未被水淹
-    #     else:
-    #         dfTot.loc[i,'是否水淹']=1#表示被水淹
-    #上面这部分可以在后期改成固定的水淹序列（先要蒙特卡洛）
+    #AMM emissions
     dfAbanRes=pd.DataFrame(index=citys,columns=timeSeries+['单位'])
     dfAbanRes['单位']='吨'
-    #以下为矿井级别
+    #mine-level
     dfAban=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfAban['lng']=dfTot['经度_百度_POI']
     dfAban['lat']=dfTot['纬度_百度_POI']
     dfAban['单位']='百吨'
     for year in timeSeries:
-        dfTemp=dfTot[(dfTot['废弃时间']<=year)&(dfTot['是否露天']==0)]#筛选废弃表
+        dfTemp=dfTot[(dfTot['废弃时间']<=year)&(dfTot['是否露天']==0)]#sheet of abandoned mines
         for city in citys:
             dfT=dfTemp[dfTemp['行政区']==city]
             UIDlist=dfT.index.tolist()
             emi=0
             for UID in UIDlist:
-                flag=0#代表不是当年废弃
+                flag=0
                 if dfT.loc[UID,'新建时间']==dfT.loc[UID,'废弃时间']:
                     flag=1
                 emiTemp=getAbanEmi(UID, year,dfT.loc[UID,'是否水淹'],flag)
                 emi=emi+emiTemp
-                dfAban.loc[UID,year]=emiTemp/100000#单位  百吨只考虑该矿井的排放，不考虑11年以前的AMM
-            dfAbanRes.loc[city,year]=(emi+iniAbanEmi(year,city))/1000#unit 吨
-        dfAbanRes.loc['广东',year]=iniAbanEmi(year,'广东')/1000#unit 吨
+                dfAban.loc[UID,year]=emiTemp/100000#unit 100t
+            dfAbanRes.loc[city,year]=(emi+iniAbanEmi(year,city))/1000#unit t
+        dfAbanRes.loc['广东',year]=iniAbanEmi(year,'广东')/1000#unit t
         dfAbanRes.loc['全国',year]=dfAbanRes[year].sum()/1000000#unit Mt或Tg
         dfAbanRes.loc['全国','单位']='Tg'
-    #计算矿后活动
+    #post mining
     dfResRes=pd.DataFrame(index=citys,columns=timeSeries+['单位'])
     dfResRes['单位']='吨'
-    #以下为矿井级别
+    #mine-level
     dfRes=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfRes['lng']=dfTot['经度_百度_POI']
     dfRes['lat']=dfTot['纬度_百度_POI']
     dfRes['单位']='百吨'
     for year in timeSeries:
-        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)]#筛选每年井工+露天表
+        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)]#inproduction underground and open-pit mines
         for city in citys:
             dfT=dfTemp[dfTemp['行政区']==city]
             UIDlist=dfT.index.tolist()
@@ -248,25 +202,24 @@ if __name__=='__main__':
             for UID in UIDlist:
                 emiTemp=getResEmi(UID, year)
                 emi=emi+emiTemp
-                dfRes.loc[UID,year]=emiTemp/100000#单位  百吨
-            dfResRes.loc[city,year]=emi/1000#unit 吨
+                dfRes.loc[UID,year]=emiTemp/100000#100t
+            dfResRes.loc[city,year]=emi/1000#unit t
         dfResRes.loc['全国',year]=dfResRes[year].sum()/1000000#unit Mt或Tg
         dfResRes.loc['全国','单位']='Tg'
-    #计算回收利用
-    #此处只计算矿井级，行政区域级别的已经计算得到
+    # recovery and utilization
     dfRev=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfRev['lng']=dfTot['经度_百度_POI']
     dfRev['lat']=dfTot['纬度_百度_POI']
     dfRev['单位']='百吨'
     for year in timeSeries:
-        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==0)]#筛选每年井工表
+        dfTemp=dfTot[(dfTot['新建时间']<=year)&(dfTot['废弃时间']>year)&(dfTot['是否露天']==0)]#in-production underground mines
         for city in citys:
             dfT=dfTemp[dfTemp['行政区']==city]
             UIDlist=dfT.index.tolist()
             emi=0
             for UID in UIDlist:
                 emiTemp=getRevEmi(UID, year)
-                dfRev.loc[UID,year]=emiTemp/100000#单位  百吨
+                dfRev.loc[UID,year]=emiTemp/100000#100t
     #Overall emissions
     dfAll=pd.DataFrame(index=idxList,columns=timeSeries+['lng','lat','单位'])
     dfAll['lng']=dfTot['经度_百度_POI']
