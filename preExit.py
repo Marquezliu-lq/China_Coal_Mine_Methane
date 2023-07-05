819b586507dbc6e1d71fbabef532d3537436d2ff
Marquezliu-lq <84384725+Marquezliu-lq@users.noreply.github.com>
2023-07-05 21:27:06 +0800

    Update preExit.py

--- preExit.py
+++ preExit.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
 
-### 退出顺序文件
+### refresh the timesheet of mine closure
 def ExitOrder():
     path1=r'煤炭退出/退出/总表.xlsx'
     path2=r'煤炭退出/退出/四个情景数据.xlsx'
@@ -24,16 +24,13 @@ def ExitOrder():
         df2=pd.read_excel(path2,'消费情景',index_col='年份')
         dfPro=pd.read_excel(path3,0,index_col='UID')
         dfOut=pd.read_excel(path4,retire,index_col='UID')
-        #清空中间表
+        #clear sheet
         for a in dfOut.columns:
             for i in dfOut.index:
                 dfOut.loc[i,a]=None
-        #安全退出路径
-        #engCon=df2.loc[2020:2050,'2度情景']#截取能源消耗序列
         years=range(2020,2051,1)
-        #按照退出顺序反向排序
-        #20220626, 这里排序应该是False, 为了算上限暂时改为True
-        df1.sort_values(by=[retire],ascending=[False],inplace=True)#先排序
+        #ranke the order of retirement
+        df1.sort_values(by=[retire],ascending=[False],inplace=True)
         
         idxList=df1.index.tolist()
     
@@ -51,12 +48,12 @@ def ExitOrder():
                         dfOut.loc[idx,sce]=year
                     else:
                         break
-        dfOut.fillna(value=10000,inplace=True)#未退出煤矿填充空值
+        dfOut.fillna(value=10000,inplace=True)#unretirement mines
         dfOut.to_excel(writer,sheet_name=retire)
     writer.save()
     return
 
-##  1108 不同路径下的校准因子
+##calibrating annual production of that year
 def CaliFactor():
     path1=r'煤炭退出/退出/退出顺序.xlsx'
     path2=r'煤炭退出/退出/四个情景数据.xlsx'
@@ -98,8 +95,7 @@ def CaliFactor():
     return
 
 
-###  价格变动和累积成本
-
+###calculating average production costs in the future and the cumulative production costs
 def CostCalc():
     path1=r'煤炭退出/退出/总表.xlsx'
     
@@ -135,71 +131,3 @@ def CostCalc():
         df.to_excel(writer,sheet_name=scenario)
     writer.save()
     return
-
-def MarCurve():
-    path1=r'煤炭退出/退出/总表.xlsx'
-    path2=r'煤炭退出/退出/临时结果/成本曲线.xlsx'
-    path3=r'煤炭退出/退出/临时结果/EF曲线.xlsx'
-    
-    df=pd.read_excel(path1,0,index_col='UID')
-    for manner in ['价格','排放因子']:
-        df.sort_values(by=[manner],ascending=[False],inplace=True)#先排序
-        
-        price=df[manner].unique()
-        dfCost=pd.DataFrame(index=price,columns=['产能'])
-        
-        for pri in price:
-            dfT=df[df[manner]==pri]
-            dfCost.loc[pri,'产能']=dfT['月产煤量'].sum()*12/100000000#亿吨
-        if manner=='价格':
-            path=path2
-        else:
-            path=path3
-        dfCost.to_excel(path,sheet_name='Sheet1')
-    return
-
-
-# ### 计算排放顺序
-# import pandas as pd
-# import math
-
-# path1=r'煤炭退出/退出/所有煤矿产量.xlsx'
-# path2=r'煤炭退出/退出/所有煤矿总表.xlsx'
-
-# dfTot=pd.read_excel(path2,0,index_col='UID')
-# dfPro=pd.read_excel(path1,0,index_col='UID')
-# b=2.017
-# Df=0.672#水淹
-# Dd=0.302#干井
-# #定义获取产量
-# def getProdAbi(UIDList,year):
-#     prod=0
-#     for UID in UIDList:
-#         if year<=2019:
-#             temp=dfPro.loc[UID,year]
-#         else:
-#             temp=dfPro.loc[UID,2019]
-            
-#         if not pd.isna(temp):
-#             prod=prod+temp
-#     return prod
-
-# ### 井工部分
-# def getMineEmi(UID,year):
-#     '''传入具体某个UID，在此基础上计算排放和'''
-#     '''返回单位：kg'''
-#     ef=dfTot.loc[UID,'排放因子']#unit kg/t
-#     prod=getProdAbi([UID],year)*12#unit t
-#     Emi=ef*prod
-#     return Emi
-
-# UIDList=dfTot[(dfTot['是否水淹']==0)&(dfTot['是否露天']==0)].index.tolist()
-# for UID in UIDList:
-#     if dfTot.loc[UID,'废弃时间'] != 10000 and dfTot.loc[UID,'废弃时间']!=dfTot.loc[UID,'新建时间']:
-#         year=dfTot.loc[UID,'废弃时间']-1
-#     elif dfTot.loc[UID,'废弃时间']==dfTot.loc[UID,'新建时间']:
-#         year=dfTot.loc[UID,'废弃时间']
-#     else:
-#         year=2019
-#     dfTot.loc[UID,'排放']=getMineEmi(UID,year)
-# dfTot.to_excel(r'煤炭退出/退出/所有煤矿总表.xlsx',sheet_name='Sheet1')
